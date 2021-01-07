a27bbbf6b8dd2200baad790b2b12adfaf9a5d405
asiyeyildirim <asiyeeyildirim71@gmail.com>
2021-01-07 14:17:57 +0300

    4 kenar döndürme işlemi
    Windows Apı'dan yararlanarak fonksiyon kullandık

---
+++ dosya1/dosya1/dosya1.cpp
@@ -0,0 +1,74 @@
+﻿#include<iostream>
+#include<Windows.h>
+using namespace std;
+
+const int genislik = 80; // sabit bir değişken oluşturduk.Nereye kadar gideceğini belirttik
+const int yukseklik = 20;
+
+
+//Konum belirtek,koordinatı değiştirmek için windows apı'den yararlanıyoruz.Bunun için fonksiyonu tanımlayalım
+
+
+void gotoxy(int x, int y)
+{
+	COORD coord;
+	coord.X = x;
+	coord.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
+
+
+
+int main()
+{
+	//soldan sağa doğru a'ları çizme işlemi
+	for (int i = 0; i < genislik; i++) {
+
+		cout << "a";
+
+		Sleep(30); //bekleme süresi belirterek çalıştırma.yavaş yavaş
+
+	}
+
+	//a'ları aşağı doğru götürme işlemi
+
+	for (int i = 0; i < yukseklik; i++)
+	{
+
+		gotoxy(genislik - 1, i);
+
+
+		cout << "a";
+		Sleep(30); //hepsini aynı anda görmemek için
+	}
+	//sağdan sola döndürme
+
+	for (int i = 0; i < genislik; i++) {
+
+		gotoxy(genislik - i - 1, yukseklik - 1);
+
+		cout << "a";
+
+		Sleep(30); //bekleme süresi belirterek çalıştırma.yavaş yavaş
+
+	}
+
+
+	for (int i = 0; i < yukseklik; i++) {
+
+		gotoxy(0, yukseklik - 1 - i);
+
+		cout << "a";
+
+		Sleep(30); //bekleme süresi belirterek çalıştırma.yavaş yavaş
+
+	}
+
+
+
+	cin.get();
+
+
+
+}
